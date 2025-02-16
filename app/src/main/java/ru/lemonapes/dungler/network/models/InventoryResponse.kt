0996e442fb098c8d0c87acd4cfac7c095726d2a1
alekse.pashkin <alekse.pashkin@x5.ru>
2025-02-16 23:16:37 +0400

    minor fix

--- app/src/main/java/ru/lemonapes/dungler/network/models/InventoryResponse.kt
+++ app/src/main/java/ru/lemonapes/dungler/network/models/InventoryResponse.kt
@@ -1,11 +1,10 @@
 package ru.lemonapes.dungler.network.models
 
-import kotlinx.collections.immutable.ImmutableList
 import kotlinx.serialization.Serializable
 import ru.lemonapes.dungler.domain_models.ReagentId
 
 @Serializable
 data class InventoryResponse(
-    val gears: ImmutableList<ServerGear>? = null,
+    val gears: List<ServerGear>? = null,
     val reagents: Map<ReagentId, Int>? = null,
 )
