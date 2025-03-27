af6bddc03ae07c4b43a045630404e7f49ae70467
Aleksei Pashkin <shkinlim@gmail.com>
2025-03-27 23:00:00 +0400

    Food creation

--- app/src/main/java/ru/lemonapes/dungler/mappers/FoodMapper.kt
+++ app/src/main/java/ru/lemonapes/dungler/mappers/FoodMapper.kt
@@ -1,13 +1,12 @@
 package ru.lemonapes.dungler.mappers
 
 import ru.lemonapes.dungler.domain_models.Food
-import ru.lemonapes.dungler.domain_models.FoodId
 import ru.lemonapes.dungler.network.models.ServerFood
 
 object FoodMapper : (ServerFood) -> Food {
     override fun invoke(serverFood: ServerFood) =
         Food(
-            id = FoodId.valueOf(serverFood.id.uppercase()),
+            id = serverFood.id,
             healthRegenAmount = serverFood.healthRegenAmount,
             count = serverFood.count
         )
--- app/src/main/java/ru/lemonapes/dungler/network/models/ServerFood.kt
+++ app/src/main/java/ru/lemonapes/dungler/network/models/ServerFood.kt
@@ -2,11 +2,12 @@ package ru.lemonapes.dungler.network.models
 
 import kotlinx.serialization.SerialName
 import kotlinx.serialization.Serializable
+import ru.lemonapes.dungler.domain_models.FoodId
 
 @Serializable
 data class ServerFood(
     @SerialName("food_id")
-    val id: String,
+    val id: FoodId,
     @SerialName("health_regen_amount")
     val healthRegenAmount: Int,
     val count: Int,
