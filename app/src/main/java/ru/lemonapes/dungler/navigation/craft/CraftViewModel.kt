322f8443317672f0ae4afce944a6a14fe970be97
Aleksei Pashkin <shkinlim@gmail.com>
2025-03-27 22:55:53 +0400

    Food creation

--- app/src/main/java/ru/lemonapes/dungler/navigation/craft/CraftViewModel.kt
+++ app/src/main/java/ru/lemonapes/dungler/navigation/craft/CraftViewModel.kt
@@ -9,6 +9,7 @@ import ru.lemonapes.dungler.domain_models.GearId
 import ru.lemonapes.dungler.mappers.CraftItemResponseMapper
 import ru.lemonapes.dungler.navigation.craft.CraftViewState.CraftSwitchState
 import ru.lemonapes.dungler.network.endpoints.getCraftItems
+import ru.lemonapes.dungler.network.endpoints.postCreateFood
 import ru.lemonapes.dungler.network.endpoints.postCreateItem
 import ru.lemonapes.dungler.network.endpoints.postUpgradeItem
 import ru.lemonapes.dungler.parent_view_model.ParentViewModel
@@ -67,8 +68,12 @@ class CraftViewModel @Inject constructor(
     }
 
     override fun actionCreateFood(food: CreateFood) = withActualState {
-        /*launch(Dispatchers.IO + ceh) {
-            val (createItems, createFood, upgradeItems, reagents, heroState) = CraftItemResponseMapper(postCreateFood(foodId = foodId))
+        launch(Dispatchers.IO + ceh) {
+            val (createItems, createFood, upgradeItems, reagents, heroState) = CraftItemResponseMapper(
+                postCreateFood(
+                    foodId = food.foodId
+                )
+            )
             heroStateRepository.setNewHeroState(heroState)
 
             updateState { state ->
@@ -79,7 +84,7 @@ class CraftViewModel @Inject constructor(
                     reagents = reagents.toPersistentMap()
                 )
             }
-        }*/
+        }
     }
 
     override fun actionUpgradeItem(gearId: GearId) = withActualState {
--- app/src/main/java/ru/lemonapes/dungler/network/endpoints/PostCraftRequests.kt
+++ app/src/main/java/ru/lemonapes/dungler/network/endpoints/PostCraftRequests.kt
@@ -5,6 +5,7 @@ import io.ktor.client.request.post
 import io.ktor.client.request.setBody
 import io.ktor.http.ContentType
 import io.ktor.http.contentType
+import ru.lemonapes.dungler.domain_models.FoodId
 import ru.lemonapes.dungler.domain_models.GearId
 import ru.lemonapes.dungler.network.ENDPOINT
 import ru.lemonapes.dungler.network.HERO_ID
@@ -12,13 +13,14 @@ import ru.lemonapes.dungler.network.HttpClientProvider
 import ru.lemonapes.dungler.network.models.CraftItemsResponse
 import ru.lemonapes.dungler.network.models.checkNetworkException
 
-private const val MAP_FIRST_PARAM = "gear_id"
+private const val GEAR_ID_PARAM = "gear_id"
+private const val FOOD_ID_PARAM = "food_id"
 
 suspend fun postCreateItem(gearId: GearId): CraftItemsResponse {
     val url = "$ENDPOINT/craft_items?hero_id=$HERO_ID"
     val response = HttpClientProvider.client.post(url) {
         contentType(ContentType.Application.Json)
-        setBody(mapOf(MAP_FIRST_PARAM to gearId))
+        setBody(mapOf(GEAR_ID_PARAM to gearId))
     }
     response.checkNetworkException()
     return response.body()
@@ -28,7 +30,17 @@ suspend fun postUpgradeItem(gearId: GearId): CraftItemsResponse {
     val url = "$ENDPOINT/upgrade_items?hero_id=$HERO_ID"
     val response = HttpClientProvider.client.post(url) {
         contentType(ContentType.Application.Json)
-        setBody(mapOf(MAP_FIRST_PARAM to gearId))
+        setBody(mapOf(GEAR_ID_PARAM to gearId))
+    }
+    response.checkNetworkException()
+    return response.body()
+}
+
+suspend fun postCreateFood(foodId: FoodId): CraftItemsResponse {
+    val url = "$ENDPOINT/create_food?hero_id=$HERO_ID"
+    val response = HttpClientProvider.client.post(url) {
+        contentType(ContentType.Application.Json)
+        setBody(mapOf(FOOD_ID_PARAM to foodId))
     }
     response.checkNetworkException()
     return response.body()
